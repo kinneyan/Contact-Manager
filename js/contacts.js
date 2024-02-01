45276db237f528cb6b2f97905e3f41e5f27a0d5f
TargetAds <157516049+TargetAds@users.noreply.github.com>
2024-02-01 11:34:36 -0500

    add searchContacts() & loadContacts(results)
    Adds the functions that will search the database for the contacts and will then update the html on the site to output the results

--- js/contacts.js
+++ js/contacts.js
@@ -32,3 +32,55 @@ function addContact() {
         console.log(err.message);
     }
 }
+
+function loadContacts(results)
+{
+    let text = "";
+    let firstNameTable;
+    let lastNameTable;
+    let totalName;
+    for (let i = 0; i<results.results.length; i++)
+    {
+	text += "<tr>";
+        totalName = results.results[i];
+        totalName = totalName.split(" ");
+        firstNameTable = totalName[0];
+        lastNameTable = totalName[1];
+        text += "<td><span>" + firstNameTable + "</span></td>";
+        text += "<td><span>" + lastNameTable + "</span></td>";
+    	text += "</tr>";
+}
+    document.getElementById("contactList").innerHTML = text;
+}
+
+function searchContacts()
+{
+    let searchFirst = document.getElementById("searchFirstName").value;
+    let searchLast = document.getElementById("searchLastName").value;
+    let searchInfo = {
+        firstName: searchFirst,
+        lastName: searchLast,
+        //userId: userId
+    }
+    let payload = JSON.stringify(searchInfo);
+    let url = apiURL + '/SearchContacts' + apiExtension;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if(this.readyState == 4 && this.status == 200)
+            {
+                let json = JSON.parse(xhr.responseText);
+                loadContacts(json);
+            }
+	};
+            xhr.send(payload);
+    }
+    catch(err)
+    {
+        console.log(err.message);
+    }
+}
