8ea1502787590e506d0e2fc80b996db82d2a1dda
kinneyan <drewkinney03@gmail.com>
2024-02-06 00:08:59 -0500

    Add contact creation

--- js/contacts.js
+++ js/contacts.js
@@ -1,4 +1,5 @@
-let contacts = {}
+let contacts = {};
+let currentContact = -1;
 
 function addContact() {
 
@@ -31,6 +32,7 @@ function addContact() {
 
 function openContact(contactId)
 {
+    currentContact = contactId;
     document.getElementById("current-fname").textContent = contacts[contactId].firstName;
     document.getElementById("current-lname").textContent = contacts[contactId].lastName;
     document.getElementById("current-phone").textContent = contacts[contactId].phoneNumber;
@@ -98,6 +100,8 @@ function editContact()
 {
     // update button
     document.getElementById("contact-edit-button").style.display = "none";
+    document.getElementById("contact-delete-button").style.display = "none";
+    document.getElementById("cancel-button").style.display = "inline-block";
     document.getElementById("contact-save-button").style.display = "inline-block";
 
     // update name labels
@@ -120,9 +124,40 @@ function editContact()
     document.getElementById("email-editor").value = document.getElementById("current-email").textContent;
 }
 
+function resetFields()
+{
+    // reset visibility
+    document.getElementById("current-name").style.display = "flex";
+    document.getElementById("fname-editor-data").style.display = "none";
+    document.getElementById("lname-editor-data").style.display = "none";
+    document.getElementById("current-phone").style.display = "inline-block";
+    document.getElementById("phone-editor").style.display = "none";
+    document.getElementById("current-email").style.display = "inline-block";
+    document.getElementById("email-editor").style.display = "none";
+    
+    document.getElementById("cancel-button").style.display = "none";
+    document.getElementById("new-contact-save").style.display = "none";
+    document.getElementById("contact-edit-button").style.display = "inline-block";
+    document.getElementById("contact-delete-button").style.display = "inline-block";
+    document.getElementById("contact-save-button").style.display = "none";
+    
+    // reset field content
+    document.getElementById("current-fname").textContent = "";
+    document.getElementById("current-lname").textContent = "";
+    document.getElementById("current-phone").textContent = "";
+    document.getElementById("current-email").textContent = "";
+
+    if (currentContact > 0) openContact(currentContact);
+    else currentContact = -1;
+}
+
 function saveEdits()
 {
     // update button
+    document.getElementById("cancel-button").style.display = "none";
+    document.getElementById("new-contact-save").style.display = "none";
+    document.getElementById("contact-delete-button").style.display = "inline-block";
+
     document.getElementById("contact-save-button").style.display = "none";
     document.getElementById("contact-edit-button").style.display = "inline-block";
     
@@ -138,13 +173,70 @@ function saveEdits()
     document.getElementById("current-email").style.display = "inline-block";
 
     // save name
-    document.getElementById("current-fname").textContent = document.getElementById("fname-editor").value;
-    document.getElementById("current-lname").textContent = document.getElementById("lname-editor").value;
+    contacts[currentContact].firstName = document.getElementById("fname-editor").value;
+    contacts[currentContact].lastName = document.getElementById("lname-editor").value;
 
     // save contact fields
-    document.getElementById("current-phone").textContent = document.getElementById("phone-editor").value;
-    document.getElementById("current-email").textContent = document.getElementById("email-editor").value;
+    contacts[currentContact].phoneNumber = document.getElementById("phone-editor").value;
+    contacts[currentContact].email = document.getElementById("email-editor").value;
 
     // update contact in database
     // DO ONCE UPDATE API ENDPOINT IS CREATED
+}
+
+function newContact()
+{
+    // update button
+    document.getElementById("contact-edit-button").style.display = "none";
+    document.getElementById("contact-delete-button").style.display = "none";
+    document.getElementById("cancel-button").style.display = "inline-block";
+    document.getElementById("new-contact-save").style.display = "inline-block";
+
+    // update name labels
+    document.getElementById("current-name").style.display = "none";
+    document.getElementById("fname-editor-data").style.display = "flex";
+    document.getElementById("lname-editor-data").style.display = "flex";
+
+    // update contact fields
+    document.getElementById("current-phone").style.display = "none";
+    document.getElementById("phone-editor").style.display = "inline-block";
+    document.getElementById("current-email").style.display = "none";
+    document.getElementById("email-editor").style.display = "inline-block";
+}
+
+function createContact()
+{
+    fields = 
+    {
+        userId: userId,
+        firstName: document.getElementById("fname-editor").value,
+        lastName: document.getElementById("lname-editor").value,
+        phone: document.getElementById("phone-editor").value,
+        email: document.getElementById("email-editor").value
+    }
+
+    let payload = JSON.stringify(fields);
+    let url = apiURL + '/AddContact' + apiExtension;
+
+    try
+    {
+        $.post(url, payload, function(data, status)
+        {
+            newId = contacts.length + 1;
+            contacts[newId] =
+            {
+                firstName: fields.firstName,
+                lastName: fields.lastName,
+                phoneNumber: fields.phone,
+                email: fields.email,
+                id: newId
+            };
+            resetFields();
+            openContact(newId);
+        });
+    }
+    catch(err)
+    {
+        console.log(err.message);
+    }
 }
