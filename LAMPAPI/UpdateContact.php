0dd7663280338d55ff4e596f61705cba64664c43
victoriaoppel <89945013+victoriaoppel@users.noreply.github.com>
2024-02-06 21:18:16 -0500

    closes #18

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,39 +1,45 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$contact = $inData["contact"];
-	$userId = $inData["userId"];
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("UPDATE Contacts SET WHERE UserId = ? AND Name = ?");
-		$stmt->bind_param("ss", $userId, $contact);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
+$userId = $inData["userId"];
+$contactId = $inData["contactId"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phone = $inData["phone"];
+$email = $inData["email"];
 
-		returnWithError("");
-	}
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE UserID=? AND ID=?");
+    $stmt->bind_param("ssssii", $firstName, $lastName, $phone, $email, $userId, $contactId);
+    $stmt->execute();
+    
+    if ($stmt->affected_rows === 0) {
+        returnWithError("No contact found for the provided ID.");
+    } else {
+        returnWithError("");
+    }
+    
+    $stmt->close();
+    $conn->close();
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
 ?>
