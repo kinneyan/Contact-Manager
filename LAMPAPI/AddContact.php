32e8a1d3670245e2854c5a0bb0b7f78405263048
victoriaoppel <89945013+victoriaoppel@users.noreply.github.com>
2024-02-06 21:42:20 -0500

    added new columns

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -6,6 +6,11 @@
 	$lastName = $inData["lastName"];
 	$phone = $inData["phone"];
 	$email = $inData["email"];
+	$location = $inData["location"];
+	$hairColor = $inData["hairColor"];
+	$eyeColor = $inData["eyeColor"];
+	$height = $inData["height"];
+
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
@@ -14,8 +19,8 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $userId);
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID, Location, HairColor, EyeColor,HeightCM) VALUES(?,?,?,?,?,?,?,?,?)");
+		$stmt->bind_param("sssssssss", $firstName, $lastName, $phone, $email, $userId, $location , $hairColor, $eyeColor, $height);
 		$stmt->execute();
 		$contactId = $stmt->insert_id;
 		$stmt->close();
